c6f4934d0da9855acc06b3f8b417ce5b85801c3e
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-19 23:58:23 +0000

    js loader
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ src/util/jsClass.js
@@ -0,0 +1,24 @@
+'use strict';
+
+export class jsClass {
+    load(j){
+        const a = document.getElementsByTagName('script'),
+            b = 'src';
+        for (let i = a.length; i > 0; i--)
+            if (a[i] && a[i].getAttribute(b) != null && a[i].getAttribute(b).indexOf(j) != -1)
+                return false;
+        const n = document.createElement('script');
+        n.setAttribute('type', 'text/javascript');
+        n.setAttribute(b, j);
+        document.getElementsByTagName('head')[0].appendChild(n)
+        return n;
+    }
+    unLoad(j){
+        const b = 'src',
+        a = document.getElementsByTagName("script");
+        for (let i = a.length; i > 0; i--)
+            if (a[i] && a[i].getAttribute(b) != null && a[i].getAttribute(b).indexOf(j) != -1)
+                a[i].parentNode.removeChild(a[i]);
+    }
+
+}
