68a48abf55588669a3287c0668fc93e73acb7d36
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-19 23:58:36 +0000

    css loader
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ src/util/cssClass.js
@@ -0,0 +1,25 @@
+'use strict';
+
+export class cssClass {
+    load(c){
+        const l = 'link',
+            a = document.getElementsByTagName(l),
+            b = 'href';
+        for (let i = a.length; i > 0; i--)
+            if (a[i] && a[i].getAttribute(b) != null && a[i].getAttribute(b).indexOf(c) != -1)
+                return false;
+        const n = document.createElement(l);
+        n.setAttribute('rel', 'stylesheet');
+        n.setAttribute(b, c);
+        document.getElementsByTagName('head')[0].appendChild(n)
+        return n;
+    }
+    unLoad(c){
+        const l = 'link',
+             b = 'href',
+             a = document.getElementsByTagName(l);
+        for (let i = a.length; i > 0; i--)
+            if (a[i] && a[i].getAttribute(b) != null && a[i].getAttribute(b).indexOf(c) != -1)
+                a[i].parentNode.removeChild(a[i]);
+    }
+}
